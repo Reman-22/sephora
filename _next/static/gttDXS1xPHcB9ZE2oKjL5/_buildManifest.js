8655bb9290e8e4b2d0bf61858f8c561c1da256fe
unknown <remanh202@gmail.com>
2026-09-07 14:20:55 +0300

    Deploy

---
+++ _next/static/gttDXS1xPHcB9ZE2oKjL5/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/sephora//_next/:path+",
+        "destination": "/sephora/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
